db0d8472817778c33791a6c16ae50c87feceaa4f
Harshit036 <76908120+Harshit036@users.noreply.github.com>
2026-04-07 16:45:19 +0530

    Add: longest-increasing-subsequence - submission-1

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int func(int index, int prev, vector<int> &nums, vector<vector<int>> &dp){
+        if(index == nums.size()+1) return 0;
+        if(dp[index][prev] != -1) return dp[index][prev];
+        int nottake = func(index+1, prev, nums, dp);
+        if(prev == 0 || nums[index-1] > nums[prev-1]){
+            return dp[index][prev] = max(nottake, 1 + func(index+1, index, nums, dp));
+        }
+        return dp[index][prev] = nottake;
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n+2, vector<int>(n+2, 0));
+        for(int index = n; index >= 1; index--){
+            for(int prev = 0; prev <= n; prev++){
+                int take = 0;
+                if(prev == 0 || nums[index-1] > nums[prev-1]){
+                    take = 1 + dp[index+1][index];
+                }
+                int nottake = dp[index+1][prev];
+                dp[index][prev] = max(take, nottake);
+            }
+        }
+        return dp[1][0];
+    }
+};
